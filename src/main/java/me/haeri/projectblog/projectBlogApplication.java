2436ff78db8c8387b6d3409bdcc33abb606044e3
haeri43 <hadi43@naver.com>
2023-11-23 16:06:03 +0900

    Initial commit

---
+++ src/main/java/me/haeri/projectblog/projectBlogApplication.java
@@ -0,0 +1,11 @@
+package me.haeri.projectblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class projectBlogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(projectBlogApplication.class, args);
+    }
+}
